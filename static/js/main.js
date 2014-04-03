f8d594784453c2eb86e136ad7c281f7b785cf6ce
jhon medina <jhofe_s17@hotmail.com>
2014-04-03 09:42:40 -0500

    aplicacion preguntas

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,44 @@
+$("#add-enlace").on('click', mostrarCuadroEnlace);
+
+function mostrarCuadroEnlace(e){
+	e.preventDefault();
+	$('#enlace').toggle();
+}
+
+$("#add-codigo").on('click', mostrarCuadroCodigo);
+
+function mostrarCuadroCodigo(e){
+	e.preventDefault();
+	$('#codigo').toggle();
+}
+
 $('.dropdown-toggle').dropdown();
+
+
+
+$('#negrita').on('click', ponerNegrita);
+
+function ponerNegrita(e){
+	e.preventDefault();
+	var cont = $('#contenido').val();
+	$('#contenido').html('<b>'+cont+'</b>');
+	console.log('negritaaaaa!!!!');
+}
+
+$('#preguntar').on('click', ponerPregunta);
+
+function ponerPregunta(e){
+	e.preventDefault();
+	var tituloPregunta = $('#titulo-p').val();
+	$('#tit-pregunta').html('<h2>'+tituloPregunta+'</h2>');
+}
+
+$('#agregar-enlace-bot').on('click',mostrarEnlace);
+
+function mostrarEnlace(e){
+	e.preventDefault();
+	var cuadroEnlace = $('#cuadro-enlace').val();
+	$('#cont-enlace').html('<a>'+cuadroEnlace+'</a>');
+}
+
+
